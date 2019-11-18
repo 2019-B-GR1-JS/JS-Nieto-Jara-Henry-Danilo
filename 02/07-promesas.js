0613ee71a78a371aef7a005db3abb43dfa08bfb3
Danilo Nieto <danilo48_its@yahoo.com>
2019-11-18 17:06:48 -0500

    Promesas sincronas

--- 02/07-promesas.js
+++ 02/07-promesas.js
@@ -22,12 +22,12 @@ console.log('01) Inicial');
 promesaSumarDosNumeros
     .then(
         (ok)=>{
-            console.log('Ok', ok)
+            //console.log('Ok', ok)
         }
     )
     .catch(
         (error) => {
-            console.log('Error', error)
+            //console.log('Error', error)
         }
     )
 
@@ -57,16 +57,63 @@ const leerArchivo = (pathArchivo)=>{
 leerArchivo('./01-variables.js')
     .then(
         (contendoCallbackjs)=>{
-            console.log(contendoCallbackjs);
+            //console.log(contendoCallbackjs);
             return leerArchivo('./01-variables.js') //promesa
         }
     )
     .then(
         (contenido2js)=>{
-            console.log(contenido2js);
+            //console.log(contenido2js);
         }
     )
-    .catch();
+    .catch(
+        (error)=> {
+            //console.log('Error',error);
+        }
+    );
+
+
+const nombreArchivo = './01-variables.js';
+//codigo sync puede producir un error
+//usar un bloque try y catch
+console.log('Inicia Syncrono')
+try{
+    const contenidoArchivo = fs.readFileSync(nombreArchivo,'utf-8')
+    console.log(contenidoArchivo)
+    console.log('Se leyo sincronamente')
+}catch(error){
+    console.log("Error",error)
+}
+console.log('Termina Syncrono')
+
+//Transformar una promesa a codigo Sincrono!
+//Function a Function Anonymous
+// 1) "async" (Permite usar codigo sincrono dentro de la function
+// 2) Para transformar Promesa -> Sincrono
+//  await
+async function leerARchivoSync() {
+    try{
+        const contenido = await leerArchivo(nombreArchivo); //promesa
+        console.log(contenido);
+        console.log('Leimos con async await');
+        return 1
+    }catch(error){
+        console.log("Error",error);
+        return 0
+    }
+}
+
+leerARchivoSync().then(
+    (numero)=>{
+        console.log(numero); // 1
+    }
+).catch(
+    (numero)=>{
+        console.log(numero); // 0
+    }
+); //Promesa
+
+
 
 
 
